ad25119746a7aabc2fa4cf37a153bd9599fcba43
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-15 18:50:53 +0200

    add test templet

---
+++ src/components/SingnInHandler.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ValidationTextFields from '../items/test';
+
+export class SignInHandler extends Component {
+    userLogin = {
+        username: "",
+        password: "",
+    };
+    inputChange = (event: any) => {
+        if (event.target.name == "username") {
+            this.userLogin.username = event.target.value;
+        } else if (event.target.name == "password") {
+            this.userLogin.password = event.target.value;
+        }
+        // console.log(this.userLogin);
+    };
+    Submite = () => {
+        console.log("submit the form");
+        if (this.verification()) {
+            console.log("----Submit valide-----");
+            console.log(this.userLogin);
+        } else {
+            console.log("----Submit not valide----");
+        }
+
+    }
+    verification = () => {
+        if (this.userLogin.username != "" && this.userLogin.password != "") {
+            return true;
+        }
+        return false;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>hi mohamed boushaba</h1>
+                <ValidationTextFields />
+            </div>
+
+        );
+
+    }
+
+
+
+
+}
